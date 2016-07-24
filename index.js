48dd867aa21bece1dbe736d04a7be19b0baaadac
敬超 <jing@turingca.com>
2016-07-24 22:59:02 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 window.onload = function(){
-  document.getElementsByClassName('btn').addEventListener("click",function(){
-  alert('你点我了，不要这样子嘛');
-});
+  document.getElementsByTagName('a').addEventListener("click",function(){
+    alert('你点我了，不要这样子嘛');
+  });
 }
 
