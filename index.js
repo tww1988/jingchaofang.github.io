e9c8811c86b7905c4af25abfbd99c8e0744dc48e
敬超 <jing@turingca.com>
2016-07-24 23:15:14 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 window.onload = function(){
-  document.getElementsByClassName('btn').addEventListener("click",function(){
+  document.getElementById('myBtn').addEventListener("click",function(){
     alert('你点我了，不要这样子嘛');
   })
 }
